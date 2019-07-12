acabbcf2967732499ac0d1accf0369ef1727a711
milan.timotijevic@symphony.is <milan.timotijevic@symphony.is>
2019-07-12 18:32:51 +0200

    removed justrun.js

--- just.run.js
+++
@@ -1,9 +0,0 @@
-const db = require('diskdb');
-let appData = require('app-data-folder');
-let appDataPath = appData('Character Tracker');
-const dbConnected = db.connect(appDataPath, ['characters', 'logs']);
-const Log = require('./utils/logger').init(db);
-
-for (let i = 0; i < 1000; i++) {
-    Log.error('La gente esta muuuuy locaaa!!');
-}
