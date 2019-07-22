d9e879fb27524d9bc937ed480e57b7da6f36a5b8
milan.timotijevic@symphony.is <milan.timotijevic@symphony.is>
2019-07-22 09:31:28 +0200

    manually check whether appdata folder exists and create it if not

--- database/index.js
+++ database/index.js
@@ -3,6 +3,16 @@
  **/
 let appData = require('app-data-folder');
 let appDataPath = appData('Character Tracker');
+const fs = require('fs');
+
+/**
+ * Checks whether App Data (or Mac equivalent) exists and creates it if not
+ * Typically, the installer should take care of this folder, but we still want to be on the safe side
+ */
+if (!fs.existsSync(appDataPath)){
+    fs.mkdirSync(appDataPath);
+}
+
 const db = require('diskdb');
 const dbConnected = db.connect(appDataPath, ['characters', 'logs', 'settings']);
 
